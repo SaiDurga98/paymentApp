93f463b23b6e0e1d44bc9fc628f1d445b371f4dd
Saidurga98 <saidurga98.devireddy@gmail.com>
2025-02-27 17:00:49 -0500

    first commit

---
+++ springBootPaymentApp/src/main/java/com/techie/springBootPaymentApp/SpringBootPaymentAppApplication.java
@@ -0,0 +1,13 @@
+package com.techie.springBootPaymentApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPaymentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPaymentAppApplication.class, args);
+	}
+
+}
---
+++ springBootPaymentApp/src/test/java/com/techie/springBootPaymentApp/SpringBootPaymentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techie.springBootPaymentApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPaymentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
